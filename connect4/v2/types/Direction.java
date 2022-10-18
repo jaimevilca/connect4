cf3160d664804dcdb4e6fdb99a62ee396bdbf2c5
jvilca <jvilca@montran.com>
2022-10-18 10:43:59 -0500

    Refactor directions values

--- connect4/v2/types/Direction.java
+++ connect4/v2/types/Direction.java
@@ -8,10 +8,10 @@ public enum Direction {
     LEFT(0, -1),
     UP(1, 0),
     DOWN(-1, 0),
-    UP_RIGHT(1, 1),
-    UP_LEFT(1, -1),
-    DOWN_RIGHT(-1, 1),
-    DOWN_LEFT(1, 1);
+    UP_RIGHT(-1, 1),
+    UP_LEFT(-1, -1),
+    DOWN_RIGHT(1, 1),
+    DOWN_LEFT(1, -1);
 
     private Coordinate incremental;
 
